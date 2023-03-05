04f1c064134985b3d37b6ee994fc8c6ac445f48c
Carlo <97054669+DeWitt46@users.noreply.github.com>
2023-03-05 23:58:06 +0100

    Add files via upload

---
+++ geant4/NaI/build/CMakeFiles/exampleB4b.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/exampleB4b.cc" "CMakeFiles/exampleB4b.dir/exampleB4b.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/exampleB4b.cc.o.d"
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/src/ActionInitialization.cc" "CMakeFiles/exampleB4b.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/ActionInitialization.cc.o.d"
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/src/DetectorConstruction.cc" "CMakeFiles/exampleB4b.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/DetectorConstruction.cc.o.d"
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/src/EventAction.cc" "CMakeFiles/exampleB4b.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/EventAction.cc.o.d"
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleB4b.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/src/RunAction.cc" "CMakeFiles/exampleB4b.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/RunAction.cc.o.d"
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/src/RunData.cc" "CMakeFiles/exampleB4b.dir/src/RunData.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/RunData.cc.o.d"
+  "/data/dataalice/corso-programmazione-avanzata/geant4-v11.0.3/examples/students/czollo/NaI/src/SteppingAction.cc" "CMakeFiles/exampleB4b.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/exampleB4b.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
